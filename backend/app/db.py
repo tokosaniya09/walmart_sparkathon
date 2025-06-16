e25527bc2a791239d9f8cf84689d694dbd67cf18
Toko saniya <tokosaniya09@gmail.com>
2025-06-16 22:48:26 +0530

    Table is created

---
+++ backend/app/db.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, create_engine, Session
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
